a1b9a6be02de10758ab172e7b09139b1cfd699f2
Preet Rajput <rajputpreet72@gmail.com>
2026-07-09 02:18:48 +0530

    updated nugget packages across all projects

--- models/Dtos/PetDtos/PetInfoDto.cs
+++ models/Dtos/PetDtos/PetInfoDto.cs
@@ -9,7 +9,7 @@ namespace models.Dtos.PetDtos
     public class PetInfoDto
     {
         public string? PetName { get; set; }
-        public string Pet_Type { get; set; }
+        public string? Pet_Type { get; set; }
         public int Pet_Level { get; set; }
     }
 }
